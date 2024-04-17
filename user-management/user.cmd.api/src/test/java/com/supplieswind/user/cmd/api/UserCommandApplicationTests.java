65d242593be1d9768771f88fcb9a84cbab2a544c
Eduardo Rasgado Ruiz <eduardorasgadoruiz@gmail.com>
2024-04-16 23:08:54 -0600

    multimodule user-management project was created and pom files were configured

---
+++ user-management/user.cmd.api/src/test/java/com/supplieswind/user/cmd/api/UserCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplieswind.user.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-management/user.query.api/src/test/java/com/supplieswind/user/query/api/UserQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplieswind.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
