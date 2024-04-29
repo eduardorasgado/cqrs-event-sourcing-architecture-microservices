723256f906ff3445a1e699c74fa47848190bd1a0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2024-04-28 20:28:25 -0600

    authentication server is working

--- user-management/user.auth/src/main/java/com/supplieswind/user/auth/config/AuthServerConfig.java
+++ user-management/user.auth/src/main/java/com/supplieswind/user/auth/config/AuthServerConfig.java
@@ -9,19 +9,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
@@ -52,6 +56,16 @@ public class AuthServerConfig {
     @Order(1)
     SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(Customizer.withDefaults());
+
+        http
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint(
+                                new LoginUrlAuthenticationEntryPoint("/login")))
+                .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()));
+
         return http.build();
     }
 
@@ -59,30 +73,38 @@ public class AuthServerConfig {
     public RegisteredClientRepository registeredClientRepository() {
         var registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId(clientId)
-                .clientSecret(new BCryptPasswordEncoder(12).encode(clientSecret))
+                .clientSecret("{noop}" + clientSecret) //new BCryptPasswordEncoder(12).encode(clientSecret)
 
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
 
-                .scope("read")
-                .scope("write")
+                .scope(OidcScopes.OPENID)
 
-                .tokenSettings(TokenSettings.builder()
-                        .accessTokenTimeToLive(Duration.ofSeconds(tokenValidityInSecs))
-                        .refreshTokenTimeToLive(Duration.ofSeconds(refreshTokenValidityInSecs))
-                        .build())
+                .tokenSettings(tokenSettings())
+                .clientSettings(clientSettings())
 
-                .clientSettings(ClientSettings.builder()
-                        .requireAuthorizationConsent(true)
-                        .build())
-
-                .redirectUri("http://127.0.0.1:8080/authorized")
+                .redirectUri("https://oauthdebugger.com/debug") // the redirection after success
 
                 .build();
 
         return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        return TokenSettings.builder()
+                .accessTokenTimeToLive(Duration.ofSeconds(tokenValidityInSecs))
+                .refreshTokenTimeToLive(Duration.ofSeconds(refreshTokenValidityInSecs))
+                .build();
+    }
+
+    @Bean
+    public ClientSettings clientSettings() {
+        return ClientSettings.builder()
+                .requireProofKey(true)
+                .build();
     }
 
     @Bean
@@ -106,27 +128,35 @@ public class AuthServerConfig {
 
     @Bean
     public JWKSource<SecurityContext> jwkSource() {
-        KeyPair keyPair = generateRsaKey();
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-                .privateKey(privateKey)
-                .keyID(UUID.randomUUID().toString())
-                .build();
-        JWKSet jwkSet = new JWKSet(rsaKey);
+        JWKSet jwkSet = new JWKSet(generateRsaKey());
         return new ImmutableJWKSet<>(jwkSet);
     }
 
-    private static KeyPair generateRsaKey() {
+    private static RSAKey generateRsaKey() {
+        KeyPair keyPair = generateKeyPair();
+
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    private static KeyPair generateKeyPair() {
         KeyPair keyPair;
+
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(2048);
+
             keyPair = keyPairGenerator.generateKeyPair();
         }
         catch (Exception ex) {
             throw new IllegalStateException(ex);
         }
+
         return keyPair;
     }
 }
